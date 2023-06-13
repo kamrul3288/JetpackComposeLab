8d39661217c852195de9ec778317d8abaa95aa18
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-06-13 11:40:56 +0600

    add compose navigation deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation (libs.androidx.compose.ui.graphics)
     implementation (libs.androidx.compose.ui.tooling.preview)
     implementation (libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.navigation)
     testImplementation (libs.junit4)
     androidTestImplementation (libs.androidx.junit)
     androidTestImplementation (libs.androidx.espresso.core)
