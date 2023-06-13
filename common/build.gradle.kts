35b43faf78ae6bb9a86bec97c40bc94e6fe3830d
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-06-13 12:59:27 +0600

    init common module

---
+++ common/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("composelab.android.library.compose")
+    id("composelab.android.library")
+}
+
+android {
+    namespace = "com.iamkamrul.common"
+}
+
+dependencies {
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,4 @@ dependencyResolutionManagement {
 }
 rootProject.name = "JetpackComposeLab"
 include(":app")
+include(":common")
