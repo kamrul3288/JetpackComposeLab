0ee5bb93e854a8d79c4696e5ffc6a35ddce6e35e
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-06-16 17:28:12 +0600

    implement text example

--- material-component/src/main/java/com/iamkamrul/material/texts/TextScreen.kt
+++ material-component/src/main/java/com/iamkamrul/material/texts/TextScreen.kt
@@ -1,11 +1,134 @@
 package com.iamkamrul.material.texts
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.iamkamrul.common.R
+import com.iamkamrul.common.theme.Pink40
+import com.iamkamrul.common.theme.Purple40
+import com.iamkamrul.common.theme.White
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TextScreen(
     onBackClick:()->Unit
 ){
+    var isShowMoreText by remember {mutableStateOf(false)}
+    var maxLine by remember {mutableStateOf(3)}
 
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "Tex Bootcamp", color = Color.White)
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        modifier = Modifier.clickable {onBackClick()},
+                        contentDescription = "back_icon", tint = White
+                    )
+                }
+            )
+        }
+    ) {
+        val modifier = Modifier.padding(it)
+        Column(modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)) {
+            Text(text = "Hello Compose")
+            
+            Text(text = "Hello Compose", style = MaterialTheme.typography.titleMedium)
+            
+            Text(text = "Hello Compose", fontSize = 16.sp, fontWeight = FontWeight.Bold, letterSpacing = 2.sp)
+
+            Text(text = stringResource(id = R.string.app_name))
+
+            Text(text = "Hello Compose", style = MaterialTheme.typography.titleMedium, color = Purple40)
+
+
+            Text(
+                buildAnnotatedString {
+                    withStyle(style = SpanStyle(color = Purple40)){
+                        append("Hello")
+                    }
+                    withStyle(style = SpanStyle(color = Pink40, fontWeight = FontWeight.Black)){
+                        append("Compose")
+                    }
+                },
+                style = MaterialTheme.typography.displayLarge
+            )
+
+            Text(
+                modifier = Modifier
+                    .background(color = Purple40, shape = RoundedCornerShape(5.dp))
+                    .padding(10.dp),
+                text = "Hello Compose",
+                style = MaterialTheme.typography.titleMedium,
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = stringResource(id = R.string.lorem),
+                maxLines =  maxLine,
+                overflow = TextOverflow.Ellipsis,
+                onTextLayout = {result->
+                    if (result.hasVisualOverflow){
+                        isShowMoreText = true
+                    }
+                }
+            )
+
+            if (isShowMoreText){
+                Button(
+                    modifier = Modifier.align(Alignment.End),
+                    onClick = {
+                        maxLine = Int.MAX_VALUE
+                        isShowMoreText = false
+                    },
+                    content = {
+                        Text(text = "More", style = MaterialTheme.typography.labelLarge)
+                    }
+                )
+            }
+
+
+
+        }
+    }
 }
