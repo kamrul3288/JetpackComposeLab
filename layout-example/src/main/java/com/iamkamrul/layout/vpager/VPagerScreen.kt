40eaa8299cf3e3521d889603bb8fee0904acf48b
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-07-10 12:05:43 +0600

    implement vertical pager screen

--- layout-example/src/main/java/com/iamkamrul/layout/vpager/VPagerScreen.kt
+++ layout-example/src/main/java/com/iamkamrul/layout/vpager/VPagerScreen.kt
@@ -1,13 +1,37 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package com.iamkamrul.layout.vpager
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.iamkamrul.common.compose.ScaffoldWithBackNavigation
 
 @Composable
 fun VerticalPagerScreen(
     onBackClick:()->Unit
 ){
+    val pagerState = rememberPagerState(initialPage = 0)
     ScaffoldWithBackNavigation(title = "Vertical Pager", onBackClick = onBackClick) {
-
+        VerticalPager(pageCount = 10, state = pagerState) {
+            Box(modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Swipe Up or Down", style = MaterialTheme.typography.labelSmall)
+                    Text(text = "Page Number - $it", style = MaterialTheme.typography.headlineLarge)
+                }
+            }
+        }
     }
 }
