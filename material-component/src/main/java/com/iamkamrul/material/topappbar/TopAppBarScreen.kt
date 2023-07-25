eca5bf910c40caa372830216fcc144dfa1fa0fcd
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-07-25 11:51:40 +0600

    implement top appbar example

--- material-component/src/main/java/com/iamkamrul/material/topappbar/TopAppBarScreen.kt
+++ material-component/src/main/java/com/iamkamrul/material/topappbar/TopAppBarScreen.kt
@@ -1,13 +1,151 @@
-package com.iamkamrul.material.topappbar
+@file:OptIn(ExperimentalMaterial3Api::class)
 
+package com.iamkamrul.material.topappbar
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Attachment
+import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.iamkamrul.common.compose.ScaffoldWithBackNavigation
+import com.iamkamrul.common.theme.White
 
 @Composable
 internal fun TopAppbarScreen(
     onBackClick:()->Unit
 ) {
     ScaffoldWithBackNavigation(title = "Top Appbar Example", onBackClick = onBackClick) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(it)
+            .padding(16.dp)) {
 
+
+            //------------------------- Default App Bar Example -------------------------------
+            Text(text = "Default App Bar Example", style = MaterialTheme.typography.labelMedium)
+            Spacer(modifier = Modifier.height(8.dp))
+            TopAppBar(
+                title = {
+                    Text(text = "Product")
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                navigationIcon = {
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Outlined.Menu, contentDescription = "Menu")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Outlined.Attachment, contentDescription = "Attachment", tint = White)
+                    }
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Outlined.AccountCircle, contentDescription = "Account", tint = White)
+                    }
+                }
+            )
+
+
+            //------------------------- Center Aligned TopAppBar Example -------------------------------
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Center Aligned TopAppBar Example", style = MaterialTheme.typography.labelMedium)
+            Spacer(modifier = Modifier.height(8.dp))
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "Product")
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                navigationIcon = {
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Outlined.Menu, contentDescription = "Menu")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Outlined.Attachment, contentDescription = "Attachment", tint = White)
+                    }
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Outlined.AccountCircle, contentDescription = "Account", tint = White)
+                    }
+                }
+            )
+
+
+            //------------------------- Medium TopAppBar Example -------------------------------
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Medium TopAppBar Example", style = MaterialTheme.typography.labelMedium)
+            Spacer(modifier = Modifier.height(8.dp))
+            MediumTopAppBar(
+                title = {
+                    Text(text = "Product")
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                navigationIcon = {
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Outlined.Menu, contentDescription = "Menu")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Outlined.Attachment, contentDescription = "Attachment", tint = White)
+                    }
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Outlined.AccountCircle, contentDescription = "Account", tint = White)
+                    }
+                }
+            )
+
+
+            //------------------------- Large TopAppBar Example -------------------------------
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Large TopAppBar Example", style = MaterialTheme.typography.labelMedium)
+            Spacer(modifier = Modifier.height(8.dp))
+            LargeTopAppBar(
+                title = {
+                    Text(text = "Product")
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                navigationIcon = {
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Outlined.Menu, contentDescription = "Menu")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Outlined.Attachment, contentDescription = "Attachment", tint = White)
+                    }
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Outlined.AccountCircle, contentDescription = "Account", tint = White)
+                    }
+                }
+            )
+
+        }
     }
 }
