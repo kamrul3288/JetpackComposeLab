232dd1b377baee663584faedc4e5de332aca9da2
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-07-31 11:46:14 +0600

    add dummy code

---
+++ material-component/src/main/java/com/iamkamrul/material/_dummy/Dummy.kt
@@ -0,0 +1,42 @@
+package com.iamkamrul.material._dummy
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.iamkamrul.common.compose.ScaffoldWithBackNavigation
+
+const val _ScreenRoute = "_ScreenRoute"
+
+fun NavController.navigateTo_Screen(){
+    this.navigate(_ScreenRoute)
+}
+
+fun NavGraphBuilder.Screen(
+    onBackClick:()->Unit
+){
+    composable(route = _ScreenRoute){
+
+    }
+}
+
+@Composable
+internal fun Screen(
+    onBackClick:()->Unit
+) {
+    ScaffoldWithBackNavigation(title = "", onBackClick = onBackClick) {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(it),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+        }
+    }
+}
