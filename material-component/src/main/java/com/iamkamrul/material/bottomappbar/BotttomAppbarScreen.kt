a244184d7ac135336b7901e510efb08468070fd2
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-08-06 11:51:31 +0600

    implement bottomAppBar screen

--- material-component/src/main/java/com/iamkamrul/material/bottomappbar/BotttomAppbarScreen.kt
+++ material-component/src/main/java/com/iamkamrul/material/bottomappbar/BotttomAppbarScreen.kt
@@ -4,6 +4,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,9 +24,37 @@ import com.iamkamrul.common.compose.ScaffoldWithBackNavigation
 internal fun BottomAppBarScreen(
     onBackClick:()->Unit
 ) {
-    ScaffoldWithBackNavigation(title = "", onBackClick = onBackClick) {
+    ScaffoldWithBackNavigation(
+        title = "Bottom Appbar",
+        onBackClick = onBackClick,
+        bottomBar = {
+            BottomAppBar(
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Default.Check, contentDescription = null)
+                    }
+
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+                    }
+
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+                    }
+                },
+                floatingActionButton = {
+                    FloatingActionButton(onClick = {}) {
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    }
+                },
+                containerColor = MaterialTheme.colorScheme.primary
+            )
+        }
+    ) {
         Column(
-            modifier = Modifier.fillMaxSize().padding(it),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
