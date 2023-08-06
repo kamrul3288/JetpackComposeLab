308662d356a7f9ce66afc89c9866c284d6b408a4
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-08-06 11:12:31 +0600

    implement checkbox screen

--- material-component/src/main/java/com/iamkamrul/material/checkbox/CheckBoxScreen.kt
+++ material-component/src/main/java/com/iamkamrul/material/checkbox/CheckBoxScreen.kt
@@ -2,9 +2,16 @@ package com.iamkamrul.material.checkbox
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.iamkamrul.common.compose.ScaffoldWithBackNavigation
@@ -13,13 +20,55 @@ import com.iamkamrul.common.compose.ScaffoldWithBackNavigation
 internal fun CheckBoxScreen(
     onBackClick:()->Unit
 ) {
-    ScaffoldWithBackNavigation(title = "", onBackClick = onBackClick) {
+    var isAppleCheckBoxChecked by remember { mutableStateOf(false) }
+    var isMangoCheckBoxChecked by remember { mutableStateOf(false) }
+    var isOrangeCheckBoxChecked by remember { mutableStateOf(false) }
+
+
+    ScaffoldWithBackNavigation(title = "CheckBox", onBackClick = onBackClick) { it ->
         Column(
-            modifier = Modifier.fillMaxSize().padding(it),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+            horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.Center
         ) {
 
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = isAppleCheckBoxChecked,
+                    onCheckedChange = {
+                        isAppleCheckBoxChecked = it
+                    }
+                )
+                Text(text = "Apple")
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = isMangoCheckBoxChecked,
+                    onCheckedChange = {
+                        isMangoCheckBoxChecked = it
+                    }
+                )
+                Text(text = "Mango")
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = isOrangeCheckBoxChecked,
+                    onCheckedChange = {
+                        isOrangeCheckBoxChecked = it
+                    }
+                )
+                Text(text = "Orange")
+            }
         }
     }
 }
