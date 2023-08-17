5fe8af8d700732b1ab6f3c11032adb7d974cf48f
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-08-17 17:28:53 +0600

    add json extension #fromJson #toJson

--- common/src/main/java/com/iamkamrul/common/extension/BundleExtension.kt
+++ common/src/main/java/com/iamkamrul/common/extension/BundleExtension.kt
@@ -2,9 +2,18 @@ package com.iamkamrul.common.extension
 
 import android.os.Build
 import android.os.Bundle
+import com.google.gson.Gson
 import java.io.Serializable
 
 fun <T : Serializable?> Bundle.getSerializableCompat(key: String, clazz: Class<T>): T {
     @Suppress("DEPRECATION")
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) getSerializable(key, clazz)!! else (getSerializable(key) as T)
+}
+
+fun <T> String.convertJsonObject(type: Class<T>): T {
+    return Gson().fromJson(this, type)
+}
+
+fun <T> T.convertJsonString(): String{
+    return Gson().toJson(this)
 }
